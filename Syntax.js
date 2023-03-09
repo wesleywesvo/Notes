6f451e4166c25c2e0ee0fa6b6009d35f3977e76a
wesleywesvo <wesleywesvo@gmail.com>
2023-03-09 14:54:19 -0500

    added join() for arrays

---
+++ Syntax.js
@@ -0,0 +1,12 @@
+# Useful methods 
+
+
+## Array.prototype.join()
+```javascript
+const a = ["wind", "water", "fire"];
+a.join();       // 'wind,water,fire'
+a.join("");     // 'windwaterfire'
+
+```
+### Parameter: specifies a string to separate each array entry 
+### Return: a string with all array elements joined 
